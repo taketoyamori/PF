346e2f1357243af74119f9f76d25faf90675efeb
yamoritaketo <ymymz87@icloud.com>
2021-01-15 16:28:07 +0900

    [add]DM機能

---
+++ app/models/message.rb
@@ -0,0 +1,7 @@
+class Message < ApplicationRecord
+
+  validates :message, presence: true
+  belongs_to :user
+  belongs_to :room
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :events, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
 
   attachment :profile_image
 
